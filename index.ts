798b0fe11e3339ad20d74dc38d9a87f386c61c57
neelishmanoja <160318744+neelishmanoja@users.noreply.github.com>
2024-03-26 08:41:35 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr /bin /env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()* 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        message: "Guess a number between 1-6:",
+        type: "number",
+        name: "userGuessedNumber",
+    }
+]);
+if(answer.userGuessedNumber === randomNumber){
+    console.log("Congrats! you won");
+}
+else{
+    console.log("you guessed wrong try again!!");
+}
